c18a13a29fb5ce302e4ff3d87a4b9a4f202bd812
Ankit Anand <ankitanand@Ankits-MacBook-Air.local>
2025-05-30 09:40:46 +0530

    auth schema redefined as type

--- src/controllers/email_verification.ts
+++ src/controllers/email_verification.ts
@@ -1,21 +1,22 @@
 import { logger } from "../config/loki.js";
-import { DeviceInterface, GPRCDeviceInterface } from "../database/interface/device_info.js";
-import { ContextInterface, EmailVerificationLabelInterface } from "../database/interface/logger.js";
-import { EmailVerificationResponse } from "../database/interface/response.js";
-import { PasswordlessAuthenticationInterface } from "../database/interface/user_passwordless_authentication.js";
+import { DeviceType, GPRCDeviceType } from "../database/types/device_info.js";
+import { ContextType, EmailVerificationLabelType } from "../database/types/logger.js";
+import { EmailVerificationResponse } from "../database/types/response.js";
+import { GPRCPasswordlessAuthenticationType, PasswordlessAuthenticationAuthType, PasswordlessAuthenticationDataType, PasswordlessAuthenticationType } from "../database/types/user_passwordless_authentication.js";
 import { userEmailVerificationRepositoriesImpl } from "../database/repositories/user_email_verification.js";
 import { userPasswordlessAuthenticationRepositories } from "../database/repositories/user_passwordless_authentication.js";
 import { Constants } from "../utils/constants.js";
 import { helper } from "../utils/helper.js";
-import { DecryptedAuthTokenInterface } from "../utils/interface.js";
-import { Utils } from "../utils/utils.js";
+import { DecryptedAuthTokenType } from "../utils/types.js";
+import { utils } from "../utils/utils.js";
+import { passwordlessAuthenticationController } from "./passwordless_authentication.js";
 
 interface UserEmailVerificationController {
-    verifyEmail(token: string, context: ContextInterface, labels: EmailVerificationLabelInterface): Promise<EmailVerificationResponse>
+    verifyEmail(token: string, context: ContextType, labels: EmailVerificationLabelType): Promise<EmailVerificationResponse>
 }
 
 class UserEmailVerificationControllerImpl implements UserEmailVerificationController {
-    async verifyEmail(token: string, context: ContextInterface, labels: EmailVerificationLabelInterface): Promise<EmailVerificationResponse> {
+    async verifyEmail(token: string, context: ContextType, labels: EmailVerificationLabelType): Promise<EmailVerificationResponse> {
         let response = new EmailVerificationResponse();
         let loggerDefaultParams = {};
         let logPayload = {
@@ -24,31 +25,34 @@ class UserEmailVerificationControllerImpl implements UserEmailVerificationContro
         };
 
         try {
-            const decryptedAuthToken: DecryptedAuthTokenInterface = helper.decryptAuthToken(token);
+            const decryptedAuthToken: DecryptedAuthTokenType = helper.decryptAuthToken(token);
 
             if(decryptedAuthToken.source === Constants.LOKI_LOGGER_LABELS.SIGNUP_REQUEST) {
                 const userResponse: EmailVerificationResponse = await userEmailVerificationRepositoriesImpl.verifyEmail(decryptedAuthToken, context, labels);
                 response = userResponse;
             }
             else if(decryptedAuthToken.source === Constants.LOKI_LOGGER_LABELS.PASSWORDLESS) {
-                const rawDeviceSchemaInfo: GPRCDeviceInterface = {
+                const rawDeviceSchemaInfo: GPRCDeviceType = {
                     deviceType: decryptedAuthToken.deviceType,
                     browserInfo: decryptedAuthToken.browserInfo,
                     ipAddress: decryptedAuthToken.ipAddress,
-                    deviceId: decryptedAuthToken.deviceId,
                     platform: decryptedAuthToken.platform,
                     deviceName: decryptedAuthToken.deviceName,
-                    loginTime: new Date(decryptedAuthToken.loginTime || Utils.CURRENT_TIME),
-                    userId: decryptedAuthToken.userId,
+                    loginTime: new Date(decryptedAuthToken.loginTime || utils.CURRENT_TIME())
                 };
-                const userSchemaInfo: PasswordlessAuthenticationInterface = {
-                    _id: decryptedAuthToken._id,
-                    username: decryptedAuthToken.username,
-                    email: decryptedAuthToken.email,
-                };  
-                const deviceSchemaInfo: DeviceInterface = helper.mapDeviceSchema(rawDeviceSchemaInfo, decryptedAuthToken.userId);
+
+                const userInfo: GPRCPasswordlessAuthenticationType = {
+                    email: decryptedAuthToken.email
+                };
+
+                const userSchemaInfo: PasswordlessAuthenticationType = {
+                    _id: helper.isNeitherNullNorUndefinedNorEmpty(decryptedAuthToken._id) ? decryptedAuthToken._id : passwordlessAuthenticationController.mapUserPasswordlessAuthenticationSchema()._id
+                };
+                const userDataSchemaInfo: PasswordlessAuthenticationDataType = passwordlessAuthenticationController.mapUserDataPasswordlessAuthenticationSchema(userInfo, userSchemaInfo._id);
+                const authDataSchemaInfo: PasswordlessAuthenticationAuthType = passwordlessAuthenticationController.mapUserAuthPasswordlessAuthenticationSchema(userInfo, userSchemaInfo._id);
+                const deviceSchemaInfo: DeviceType = helper.mapDeviceSchema(rawDeviceSchemaInfo, userSchemaInfo._id);
                 
-                const userResponse: EmailVerificationResponse = await userPasswordlessAuthenticationRepositories.createUser(userSchemaInfo, deviceSchemaInfo, context, labels);
+                const userResponse: EmailVerificationResponse = await userPasswordlessAuthenticationRepositories.createUser(userSchemaInfo, userDataSchemaInfo, authDataSchemaInfo, deviceSchemaInfo, context, labels);
                 response = userResponse;
             }
         }
